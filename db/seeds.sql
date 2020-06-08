f7d8f0db56ff21d870c06c3530b6ebb6d720cf55
John Cosper <Johngcosper@gmail.com>
2020-06-08 15:23:29 -0400

    set up db

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", true);
